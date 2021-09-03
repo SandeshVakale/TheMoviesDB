87cb6e99efc0fa6465e009ccd8f959d4814514a9
SandeshVakale <sandyrv.vakale@gmail.com>
2021-09-03 12:26:08 +0200

    changes for tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,105 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
-import App from '../src/App';
-
-// Note: test renderer must be required after react-native.
+import HomeScreen from '../src/Screens/Home';
+import DetailScreen from '../src/Screens/Detail';
 import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
+import {movieModel} from '../src/Store/Movie';
+jest.useFakeTimers();
+jest.mock('react-native-elements', () => {
+  return {
+    SearchBar: () => {
+      return <></>;
+    },
+    Text: () => {
+      return <></>;
+    },
+    Card: () => {
+      return <></>;
+    },
+    Divider: () => {
+      return <></>;
+    },
+    Image: () => {
+      return <></>;
+    },
+  };
+});
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => jest.fn(),
+}));
+const mockStore = configureStore([]);
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('Home screen renders correctly', () => {
+  const initialStore = {
+    searchModel: {movies: []},
+  };
+  let store = mockStore(initialStore);
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <HomeScreen />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Home screen renders correctly', () => {
+  const initialStore = {
+    searchModel: {
+      movies: {
+        total_results: 0,
+        results: [
+          {
+            adult: false,
+            backdrop_path: '/zJjI78peUFRIe2NPHthVhow2f60.jpg',
+            genre_ids: [28, 18],
+            id: 256040,
+            original_language: 'te',
+            original_title: 'బాహుబలి:ద బిగినింగ్',
+            overview:
+              'The young Shivudu is left as a foundling in a small village by his mother. By the time he’s grown up, it has become apparent that he possesses exceptional gifts. He meets the beautiful warrior princess Avanthika and learns that her queen has been held captive for the last 25 years. Shividu sets off to rescue her, discovering his own origins in the process.',
+            popularity: 38.573,
+            poster_path: '/9BAjt8nSSms62uOVYn1t3C3dVto.jpg',
+            release_date: '2015-07-10',
+            title: 'Bāhubali: The Beginning',
+            video: false,
+            vote_average: 7.5,
+          },
+        ],
+      },
+    },
+  };
+  let store = mockStore(initialStore);
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <HomeScreen />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Detail screen renders correctly', () => {
+  const initialStore = {
+    movieModel: {movie: {}},
+  };
+  const route = {
+    params: {
+      id: 10,
+      name: 'something',
+    },
+  };
+  let store = mockStore(initialStore);
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <DetailScreen route={route} />
+      </Provider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -33,39 +33,41 @@ function HomeScreen() {
       {movies?.total_results === 0 ? (
         <Text style={{textAlign: 'center'}}>No Results found for {query}</Text>
       ) : (
-        <FlatList
-          data={movies?.results}
-          renderItem={(item, index) => (
-            <TouchableNativeFeedback
-              onPress={() =>
-                navigation.navigate('Detail', {
-                  id: item.item.id,
-                  name: item.item.original_title,
-                })
-              }>
-              <Card key={index}>
-                <Card.FeaturedTitle
-                  style={{color: 'black', textAlign: 'center'}}>
-                  {item.item.original_title}
-                </Card.FeaturedTitle>
-                <Card.Divider />
-                <Image
-                  style={styles.image}
-                  resizeMode="contain"
-                  source={{uri: Image_URL + item.item.backdrop_path}}
-                />
-                <Card.Divider />
-                <Card.Title style={{color: 'black'}}>
-                  {moment(item.item.release_date).format('MMM DD, YYYY')}
-                </Card.Title>
-                <Card.Divider />
-                <Card.FeaturedSubtitle style={{color: 'black'}}>
-                  {item.item.overview}
-                </Card.FeaturedSubtitle>
-              </Card>
-            </TouchableNativeFeedback>
-          )}
-        />
+        movies?.results !== undefined && (
+          <FlatList
+            data={movies?.results}
+            renderItem={(item, index) => (
+              <TouchableNativeFeedback
+                onPress={() =>
+                  navigation.navigate('Detail', {
+                    id: item.item.id,
+                    name: item.item.original_title,
+                  })
+                }>
+                <Card key={index}>
+                  <Card.FeaturedTitle
+                    style={{color: 'black', textAlign: 'center'}}>
+                    {item.item.original_title}
+                  </Card.FeaturedTitle>
+                  <Card.Divider />
+                  <Image
+                    style={styles.image}
+                    resizeMode="contain"
+                    source={{uri: Image_URL + item.item.backdrop_path}}
+                  />
+                  <Card.Divider />
+                  <Card.Title style={{color: 'black'}}>
+                    {moment(item.item.release_date).format('MMM DD, YYYY')}
+                  </Card.Title>
+                  <Card.Divider />
+                  <Card.FeaturedSubtitle style={{color: 'black'}}>
+                    {item.item.overview}
+                  </Card.FeaturedSubtitle>
+                </Card>
+              </TouchableNativeFeedback>
+            )}
+          />
+        )
       )}
     </SafeAreaView>
   );
